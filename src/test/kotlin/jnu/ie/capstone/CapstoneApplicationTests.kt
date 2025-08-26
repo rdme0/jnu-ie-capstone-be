ff0f12bc6b9f5429a5d272418f470e208bfc684e
rdme0 <we6610@gmail.com>
2025-08-26 21:20:09 +0900

    :sparkles: feat: initial commit

---
+++ src/test/kotlin/jnu/ie/capstone/CapstoneApplicationTests.kt
@@ -0,0 +1,13 @@
+package jnu.ie.capstone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
