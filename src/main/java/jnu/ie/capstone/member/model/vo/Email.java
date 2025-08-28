c3ce0900a308b5ba9c8d9ad34a0f811bcc46b528
rdme0 <we6610@gmail.com>
2025-08-29 00:15:54 +0900

    :sparkles: feat: vo 작성

---
+++ src/main/java/jnu/ie/capstone/member/model/vo/Email.java
@@ -0,0 +1,35 @@
+package jnu.ie.capstone.member.model.vo;
+
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embeddable;
+import java.util.regex.Pattern;
+import jnu.ie.capstone.common.converter.StringEncryptConverter;
+import jnu.ie.capstone.common.exception.client.BadDataSyntaxException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Email {
+    @Convert(converter = StringEncryptConverter.class)
+    private String value;
+
+    public Email(String value) {
+        validateEmail(value);
+        this.value = value;
+    }
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+    );
+
+
+
+    private void validateEmail(String value) {
+        if (value == null || !EMAIL_PATTERN.matcher(value).matches()) {
+            throw new BadDataSyntaxException("올바른 이메일 형식이 아닙니다.");
+        }
+    }
+}
