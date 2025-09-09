f1bc79a2bc6ce8e5ab202d54eb84e47185c4c8f9
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-09-09 21:23:58 +0900

    :sparkles: feat: NoArgsConstructor

--- src/main/java/jnu/ie/capstone/rtzr/cache/entity/RtzrAccessToken.java
+++ src/main/java/jnu/ie/capstone/rtzr/cache/entity/RtzrAccessToken.java
@@ -1,13 +1,16 @@
 package jnu.ie.capstone.rtzr.cache.entity;
 
-import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash(value = "AccessToken", timeToLive = 3600 * 6 - 60)
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RtzrAccessToken {
 
     @Id
