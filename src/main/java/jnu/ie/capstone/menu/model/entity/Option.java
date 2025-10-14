7e7d8fe0e0cca32ec3a07c7cc569d479486ab55f
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-10-14 21:12:56 +0900

    :bug: fix: GeneratedValue 어노테이션 누락 수정

--- src/main/java/jnu/ie/capstone/menu/model/entity/Option.java
+++ src/main/java/jnu/ie/capstone/menu/model/entity/Option.java
@@ -4,6 +4,8 @@ import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -20,6 +22,7 @@ import lombok.NoArgsConstructor;
 public class Option {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
