ca7b654ae2d7d4b1d120a7d7455408fe4345f0f0
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-10-15 19:21:06 +0900

    :recycle: refactor: class -> record

--- src/main/java/jnu/ie/capstone/member/model/vo/Email.java
+++ src/main/java/jnu/ie/capstone/member/model/vo/Email.java
@@ -2,49 +2,27 @@ package jnu.ie.capstone.member.model.vo;
 
 import jakarta.persistence.Convert;
 import jakarta.persistence.Embeddable;
-import java.util.Objects;
 import java.util.regex.Pattern;
 import jnu.ie.capstone.common.converter.StringEncryptConverter;
 import jnu.ie.capstone.common.exception.client.BadDataSyntaxException;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Embeddable
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Email {
-    @Convert(converter = StringEncryptConverter.class)
-    private String value;
-
-    public Email(String value) {
-        validateEmail(value);
-        this.value = value;
-    }
+public record Email(
+        @Convert(converter = StringEncryptConverter.class)
+        String value
+) {
 
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
             "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
     );
 
+    public Email {
+        validate(value);
+    }
 
-
-    private void validateEmail(String value) {
+    private static void validate(String value) {
         if (value == null || !EMAIL_PATTERN.matcher(value).matches()) {
             throw new BadDataSyntaxException("올바른 이메일 형식이 아닙니다.");
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Email email = (Email) o;
-        return Objects.equals(getValue(), email.getValue());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getValue());
     }
 }
