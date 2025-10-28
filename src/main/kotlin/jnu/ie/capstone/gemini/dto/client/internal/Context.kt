afc965af2fb3d42b7c52bc9f29128e99b5d05f01
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-10-28 21:23:20 +0900

    :sparkles: feat: Cart 확인 컨텍스트, 결제 확인 컨텍스트 추가

--- src/main/kotlin/jnu/ie/capstone/gemini/dto/client/internal/Context.kt
+++ src/main/kotlin/jnu/ie/capstone/gemini/dto/client/internal/Context.kt
@@ -2,21 +2,51 @@ package jnu.ie.capstone.gemini.dto.client.internal
 
 import jnu.ie.capstone.menu.dto.internal.MenuInternalDTO
 import jnu.ie.capstone.session.dto.internal.ShoppingCartDTO
+import jnu.ie.capstone.session.enums.SessionState
 
 sealed interface Context {
-    data class MenuAndShoppingCart(
+    companion object {
+        val DIVIDER = "\n" + "=".repeat(20) + "\n"
+    }
+
+    data class MenuSelectionContext(
         val menus: List<MenuInternalDTO>,
-        val shoppingCart: ShoppingCartDTO
+        val shoppingCart: ShoppingCartDTO,
+        val nowState: SessionState = SessionState.MENU_SELECTION
     ) : Context {
         override fun toString(): String {
-            val menusText =  menus
+            val menusText = menus
                 .joinToString("\n" + "_".repeat(20) + "\n") { it.toString() }
-
-            val divider = "\n" + "=".repeat(20) + "\n"
 
             val shoppingCartText = "장바구니 : [\n${shoppingCart}\n]"
 
-            return menusText + divider + shoppingCartText
+            return menusText + DIVIDER + shoppingCartText + DIVIDER + "현재 상태 : " + nowState
         }
+    }
+
+    data class CartConfirmationContext(
+        val shoppingCart: ShoppingCartDTO,
+        val nowState: SessionState = SessionState.CART_CONFIRMATION,
+    ) : Context {
+        override fun toString(): String {
+            val shoppingCartText = "장바구니 : [\n${shoppingCart}\n]"
+
+            return shoppingCartText + DIVIDER + "현재 상태 : " + nowState
+        }
+    }
+
+    data class PaymentConfirmationContext(
+        val shoppingCart: ShoppingCartDTO,
+        val nowState: SessionState = SessionState.PAYMENT_CONFIRMATION,
+    ) : Context {
+        override fun toString(): String {
+            val shoppingCartText = "장바구니 : [\n${shoppingCart}\n]"
+
+            return shoppingCartText + DIVIDER + "현재 상태 : " + nowState
+        }
+    }
+
+    object NoContext : Context {
+        override fun toString() = "NoContext"
     }
 }
