6f7ec8a5f95e9e7170f99cec319a5163933ac1fb
rdme0 <we6610@gmail.com>
2025-10-31 03:06:31 +0900

    :sparkles: feat: prod 이니셜라이저 구현

---
+++ src/main/kotlin/jnu/ie/capstone/member/initializer/MemberProdInitializer.kt
@@ -0,0 +1,47 @@
+package jnu.ie.capstone.member.initializer
+
+import jnu.ie.capstone.common.security.util.JwtUtil
+import jnu.ie.capstone.member.constant.MemberConstant.TEST_EMAIL
+import jnu.ie.capstone.member.dto.MemberInfo
+import jnu.ie.capstone.member.model.entity.Member
+import jnu.ie.capstone.member.model.vo.Oauth2Provider
+import jnu.ie.capstone.member.repository.MemberRepository
+import mu.KotlinLogging
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Profile
+import org.springframework.context.event.EventListener
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+@Order(1)
+@Profile("prod")
+class MemberProdInitializer(
+    private val memberRepository: MemberRepository,
+    private val jwtUtil: JwtUtil
+) {
+
+    private companion object {
+        val logger = KotlinLogging.logger {}
+    }
+
+    @Transactional
+    @EventListener(ApplicationReadyEvent::class)
+    fun init() {
+        val member = memberRepository.findByEmail(TEST_EMAIL)
+            ?: run {
+                val testMember = Member.builder()
+                    .provider(Oauth2Provider.KAKAO)
+                    .email(TEST_EMAIL)
+                    .build()
+
+                memberRepository.save(testMember)
+            }
+
+        val accessToken = MemberInfo.from(member).let { jwtUtil.generateToken(it) }
+
+        logger.info { "테스트 member accessToken -> $accessToken" }
+    }
+
+}
