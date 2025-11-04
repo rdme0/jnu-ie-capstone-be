6aed0e1f1a70f1e6570f9005532cc7d7332d0bd4
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-11-04 16:59:54 +0900

    :recycle: refactor: 더 일반적인 이름으로 변경 (auth -> uri security)

--- src/main/kotlin/jnu/ie/capstone/common/security/config/AllowedOriginsProperties.kt
+++
@@ -1,6 +0,0 @@
-package jnu.ie.capstone.common.security.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "auth")
-data class AllowedOriginsProperties(val allowedFrontEndOrigins: List<String>)
