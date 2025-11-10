b94dc735feb1ea2c4cac349d5702c8d9391f094e
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-11-11 01:32:53 +0900

    :sparkles: feat: 인터셉터 스토어 아이디 검증 부분 추가

--- src/main/kotlin/jnu/ie/capstone/common/websocket/interceptor/StoreIdHandshakeInterceptor.kt
+++ src/main/kotlin/jnu/ie/capstone/common/websocket/interceptor/StoreIdHandshakeInterceptor.kt
@@ -1,14 +1,23 @@
 package jnu.ie.capstone.common.websocket.interceptor
 
+import jnu.ie.capstone.common.security.dto.KioskUserDetails
+import jnu.ie.capstone.store.service.StoreService
+import mu.KotlinLogging
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.server.HandshakeInterceptor
 import java.lang.Exception
 
 @Component
-class StoreIdHandshakeInterceptor : HandshakeInterceptor {
+class StoreIdHandshakeInterceptor(
+    private val storeService: StoreService
+) : HandshakeInterceptor {
+    private companion object {
+        val logger = KotlinLogging.logger {}
+    }
 
     override fun beforeHandshake(
         request: ServerHttpRequest,
@@ -18,9 +27,23 @@ class StoreIdHandshakeInterceptor : HandshakeInterceptor {
     ): Boolean {
         val uriTemplateVars = request.uri.path.split("/")
         val storeIdIndex = uriTemplateVars.indexOf("stores") + 1
+        val authentication = attributes["principal"] as? UsernamePasswordAuthenticationToken
+
+        val userDetails = authentication?.principal as? KioskUserDetails
+            ?: run {
+                logger.error { "올바르지 않은 authentication -> ${attributes["principal"]}" }
+                return false
+            }
 
         if (storeIdIndex > 0 && storeIdIndex < uriTemplateVars.size) {
             val storeId = uriTemplateVars[storeIdIndex].toLongOrNull() ?: return false
+
+            storeService.getBy(id = storeId, ownerId = userDetails.memberInfo.id)
+                ?: run {
+                    logger.warn { "올바르지 않은 store id -> ${storeId}, user -> ${userDetails.memberInfo}" }
+                    return false
+                }
+
             attributes["storeId"] = storeId
             return true
         }
