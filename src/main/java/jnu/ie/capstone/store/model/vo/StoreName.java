55e84982dd4049291ab47d33855e3fbc6513a97b
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-11-18 15:16:57 +0900

    :sparkles: feat: JsonValue 어노테이션 추가

--- src/main/java/jnu/ie/capstone/store/model/vo/StoreName.java
+++ src/main/java/jnu/ie/capstone/store/model/vo/StoreName.java
@@ -4,12 +4,13 @@ import static jnu.ie.capstone.store.constant.StoreConstant.STORE_NAME_MAX_LENGTH
 import static jnu.ie.capstone.store.constant.StoreConstant.STORE_NAME_MIN_LENGTH;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import jakarta.persistence.Embeddable;
 import jnu.ie.capstone.common.exception.client.BadDataLengthException;
 
 @Embeddable
 public record StoreName(
-        String value
+        @JsonValue String value
 ) {
 
     @JsonCreator
@@ -20,7 +21,8 @@ public record StoreName(
     private void validate(String value) {
         if (value == null || value.length() < STORE_NAME_MIN_LENGTH
                 || value.length() > STORE_NAME_MAX_LENGTH) {
-            throw new BadDataLengthException("스토어 이름", STORE_NAME_MIN_LENGTH, STORE_NAME_MAX_LENGTH);
+            throw new BadDataLengthException("스토어 이름", STORE_NAME_MIN_LENGTH,
+                    STORE_NAME_MAX_LENGTH);
         }
     }
 }
