9369d1e5e2b6d1bf089b9c9d3b41ad3c7f3412dc
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-11-30 22:40:18 +0900

    :sparkles: feat: tool response 추가

--- src/main/kotlin/jnu/ie/capstone/gemini/dto/client/request/GeminiInput.kt
+++ src/main/kotlin/jnu/ie/capstone/gemini/dto/client/request/GeminiInput.kt
@@ -3,6 +3,13 @@ package jnu.ie.capstone.gemini.dto.client.request
 import jnu.ie.capstone.gemini.dto.client.internal.Context
 
 sealed class GeminiInput {
+    @Suppress("ArrayInDataClass")
     data class Audio(val chunk: ByteArray) : GeminiInput()
     data class Text(val context: Context) : GeminiInput()
+
+    data class ToolResponse(
+        val id: String?,
+        val functionName: String,
+        val result: String
+    ) : GeminiInput()
 }
