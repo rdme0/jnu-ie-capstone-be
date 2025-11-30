d98754436ef3f50513e158864dd73a43306f58a0
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-11-30 22:37:14 +0900

    :fire: remove: rtzr stt 제거

--- src/main/kotlin/jnu/ie/capstone/rtzr/cache/repository/RtzrAccessTokenRepository.kt
+++
@@ -1,9 +0,0 @@
-package jnu.ie.capstone.rtzr.cache.repository
-
-import jnu.ie.capstone.rtzr.cache.entity.RtzrAccessToken
-import org.springframework.data.repository.CrudRepository
-import java.util.Optional
-
-interface RtzrAccessTokenRepository : CrudRepository<RtzrAccessToken, String> {
-    override fun findById(id: String) : Optional<RtzrAccessToken>
-}
